b03356258b5c0c0c8e95a39c3531212849ae6fdc
Anwar Hossain <anwar.sarker@bjitgroup.com>
2022-06-17 16:39:30 +0600

    crfeate context and design nav home and footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
